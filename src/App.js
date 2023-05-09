0eb9ca81a3d8f90b376efd417646627754faa1d3
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-05-08 17:41:45 -0700

    created file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Header from './Header';
+import List from './List';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>I am a paragraph</p>
+      <Header headerText="Banana" /> {/* let component = new Header({headerText: 'Banana'}) */}
+      <List data={['Item One', 'Item Two']} />
+      <Footer year="2023" /> {/* View += Footer(state) */ }
     </div>
   );
 }
